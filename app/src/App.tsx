de0ffc3680310103fdd6b5d83d987bddea315273
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-10-28 18:29:44 -0500

    app: tailwind setup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,7 +3,11 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  return <div>hello world</div>
+  return (
+    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      hello world
+    </div>
+  )
 }
 
 export default App
