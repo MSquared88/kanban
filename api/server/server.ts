8b327cf14f167a3e42833c9b88d79fa708ff6cf9
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-11-04 17:38:51 -0500

    api: updated server middleware

--- api/server/server.ts
+++ api/server/server.ts
@@ -3,15 +3,46 @@ import express, { Express } from "express";
 //middleware
 import helmet from "helmet";
 import cors from "cors";
-const logger = require("./Utils/logger");
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/notFound.middleware";
+import logger from "./middleware/logger.middleware";
+import { jwtCheck } from "./middleware/jwtCheck.middleware";
 
 //initiate server
 const server: Express = express();
 
 // global middleware
 server.use(express.json());
-server.use(helmet());
-server.use(cors());
+server.set("json spaces", 2);
 server.use(logger);
 
+const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL;
+server.use(
+  cors({
+    origin: CLIENT_ORIGIN_URL,
+    methods: ["GET"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    maxAge: 86400,
+  })
+);
+
+server.use(
+  helmet({
+    hsts: {
+      maxAge: 31536000,
+    },
+    contentSecurityPolicy: {
+      useDefaults: false,
+      directives: {
+        "default-src": ["'none'"],
+        "frame-ancestors": ["'none'"],
+      },
+    },
+    frameguard: {
+      action: "deny",
+    },
+  })
+);
+
+server.use(jwtCheck);
 export default server;
