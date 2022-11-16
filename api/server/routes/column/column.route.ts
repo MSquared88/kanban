c2873f8a927199837d51bd9c6bf6305b29cf5c00
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-11-15 19:17:10 -0600

    add column update and delete model and routes

--- api/server/routes/column/column.route.ts
+++ api/server/routes/column/column.route.ts
@@ -1,6 +1,6 @@
 import * as columnModel from "./column.model";
 import express, { Express, Router, Request, Response } from "express";
-import { Board, prisma } from "@prisma/client";
+import { Board } from "@prisma/client";
 
 export const router: Router = express.Router();
 
@@ -14,5 +14,44 @@ router.post("/column", async (request: Request, response: Response) => {
   } catch (error) {
     console.log(error);
     response.status(500).json({ message: "could not add column", error });
+  }
+});
+
+router.put("/column", async (request: Request, response: Response) => {
+  const { id, name, column } = request.body;
+
+  if (!id || !name)
+    return response
+      .status(500)
+      .json({ message: "id and name are required to delete column" });
+  console.log(request.body);
+  if (!column)
+    return response
+      .status(500)
+      .json({ message: "updated column is required in body" });
+  console.log(request.body);
+  try {
+    const updatedColumn = await columnModel.updateColumn(id, name, column);
+    response.status(200).json(updatedColumn);
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: "could not update column", error });
+  }
+});
+
+router.delete("/column", async (request: Request, response: Response) => {
+  const { id, name } = request.body;
+
+  if (!id || !name)
+    return response
+      .status(500)
+      .json({ message: "id and name are required to delete column" });
+
+  try {
+    const column = await columnModel.removeColumn(id, name);
+    response.status(200).json(column);
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: "could not delete column", error });
   }
 });
