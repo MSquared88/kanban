f436aa917321b021a8d446a7ed1fbf0d351c2a57
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-11-16 18:53:50 -0600

    complete crud for board model and route

--- api/server/routes/board/board.model.ts
+++ api/server/routes/board/board.model.ts
@@ -1,43 +1,37 @@
-import type {
-  Board,
-  Column,
-  Task,
-  Subtask,
-  PrismaPromise,
-} from "@prisma/client";
+import type {Board, Column, Task, Subtask, PrismaPromise} from '@prisma/client'
 
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient} from '@prisma/client'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
-export async function getBoards(ownerId: Board["ownerId"]) {
+export async function getBoards(ownerId: Board['ownerId']) {
   return prisma.board.findMany({
     where: {
       ownerId,
     },
     //includes joined tables
     include: {
-      columns: { include: { tasks: { include: { subtasks: true } } } },
+      columns: {include: {tasks: {include: {subtasks: true}}}},
     },
-  });
+  })
 }
 
 export async function addBoard(
-  ownerId: Board["ownerId"],
-  name: Board["name"],
-  columns?: Column[]
+  ownerId: Board['ownerId'],
+  name: Board['name'],
+  columns?: Column[],
 ) {
   if (columns) {
     return prisma.board.create({
       data: {
         ownerId,
         name,
-        columns: { create: [...columns] },
+        columns: {create: [...columns]},
       },
       include: {
         columns: true, // Include all columns in the returned object
       },
-    });
+    })
   }
 
   return prisma.board.create({
@@ -48,5 +42,14 @@ export async function addBoard(
     include: {
       columns: true,
     },
-  });
+  })
+}
+
+export async function updateBoard(id: Board['id'], board: Board) {
+  console.log(board)
+  return prisma.board.update({where: {id}, data: {...board}})
+}
+
+export async function removeBoard(id: Board['id']) {
+  return prisma.board.delete({where: {id}})
 }
