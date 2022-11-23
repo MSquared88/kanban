15507bd19157da4e3e771d6dacf27111eb809e85
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-11-22 21:21:58 -0600

    app setup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,11 +2,43 @@ import {useAuth0} from '@auth0/auth0-react'
 import React from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {PageLoader} from './components/PageLoader'
-import {ProtectedRoute} from './components/ProtectedRoute'
+import {ProtectedRoute} from './pages/ProtectedRoute'
+import {ProtectedPage} from './pages/ProtectedPage'
+import {Layout} from './components/layout/Layout'
 
-const AdminPage = () => <div>admin</div>
-const ProtectedPage = () => <div>ProtectedPage</div>
 const NotFoundPage = () => <div>not found</div>
+
+export const LogoutButton: React.FC = () => {
+  const {logout, user} = useAuth0()
+  const handleLogout = () => {
+    logout({
+      returnTo: window.location.origin,
+    })
+  }
+
+  return (
+    <button className="button__logout" onClick={handleLogout}>
+      Log Out
+    </button>
+  )
+}
+export const LoginButton: React.FC = () => {
+  const {loginWithRedirect} = useAuth0()
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      prompt: 'login',
+      appState: {
+        returnTo: '/protected',
+      },
+    })
+  }
+
+  return (
+    <button className="button__login" onClick={handleLogin}>
+      Log In
+    </button>
+  )
+}
 
 export const App: React.FC = () => {
   const {isLoading} = useAuth0()
@@ -21,16 +53,22 @@ export const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>home</div>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <div className="bg-red-300 font-jakarta dark:bg-black">
+                <LogoutButton /> <LoginButton />
+              </div>
+            </Layout>
+          }
+        />
 
         <Route
           path="/protected"
           element={<ProtectedRoute component={ProtectedPage} />}
         />
-        <Route
-          path="/admin"
-          element={<ProtectedRoute component={AdminPage} />}
-        />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
