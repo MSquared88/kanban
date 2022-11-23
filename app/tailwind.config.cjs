a5b0526c1c60e75bed51fff3530c663053817599
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-11-22 21:48:03 -0600

    add colors to tailwind config

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -4,11 +4,31 @@ module.exports = {
   darkMode: 'class',
   theme: {
     fontFamily: {
-      "jakarta": ['"Plus Jakarta Sans"', 'sans-serif']
+      jakarta: ['"Plus Jakarta Sans"', 'sans-serif'],
     },
-    extend: {colors: {
-
-    }},
+    extend: {
+      colors: {
+        purple: {
+          primary: '#635FC7',
+          hover: '#A8A4FF',
+        },
+        black: '#000112',
+        red: {
+          primary: '#EA5555',
+          hover: '#FF9898',
+        },
+        gray: {
+          darkest: '#20212C',
+          dark: '#2B2C37',
+          medium: '#828FA3',
+          light: '#F4F7FD',
+        },
+        lines: {
+          dark: '#3E3F4E',
+          light: '#E4EBFA',
+        },
+      },
+    },
   },
   plugins: [],
 }
