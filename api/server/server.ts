5a6e84d1d12e02a1a1ffcef557e471bccbafd87f
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-12-05 18:45:31 -0600

    add task subtask routes

--- api/server/server.ts
+++ api/server/server.ts
@@ -1,26 +1,29 @@
-import express, { Express, Router } from "express";
+import express, {Express, Router} from 'express'
 
 //middleware
-import helmet from "helmet";
-import cors from "cors";
-import { errorHandler } from "./middleware/error.middleware";
-import { notFoundHandler } from "./middleware/notFound.middleware";
-import logger from "./middleware/logger.middleware";
-import { jwtCheck } from "./middleware/jwtCheck.middleware";
+import helmet from 'helmet'
+import cors from 'cors'
+import {errorHandler} from './middleware/error.middleware'
+import {notFoundHandler} from './middleware/notFound.middleware'
+import logger from './middleware/logger.middleware'
+import {jwtCheck} from './middleware/auth/jwtCheck.middleware'
 
 //routes
-import { router as boardRouter } from "./routes/board/board.route";
-import { router as columnRouter } from "./routes/column/column.route";
+import {router as boardRouter} from './routes/board/board.route'
+import {router as columnRouter} from './routes/column/column.route'
+import {router as taskRouter} from './routes/task/task.route'
+import {router as subtaskRouter} from './routes/subtask/subtask.route'
+
 //initiate server
-const server: Express = express();
+const server: Express = express()
 
 // global middleware
-server.use(express.json());
-server.set("json spaces", 2);
-server.use(logger);
+server.use(express.json())
+server.set('json spaces', 2)
+server.use(logger)
 
-const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL;
-server.use(cors());
+const CLIENT_ORIGIN_URL = process.env.CLIENT_ORIGIN_URL
+server.use(cors())
 
 server.use(
   helmet({
@@ -30,27 +33,29 @@ server.use(
     contentSecurityPolicy: {
       useDefaults: false,
       directives: {
-        "default-src": ["'none'"],
-        "frame-ancestors": ["'none'"],
+        'default-src': ["'none'"],
+        'frame-ancestors': ["'none'"],
       },
     },
     frameguard: {
-      action: "deny",
+      action: 'deny',
     },
-  })
-);
-server.use(errorHandler);
+  }),
+)
+server.use(errorHandler)
 
-server.get("/", (request, response) => {
-  response.status(200).json({ message: "success" });
-});
-server.use("/api", boardRouter);
-server.use("/api", columnRouter);
+server.get('/', (request, response) => {
+  response.status(200).json({message: 'success'})
+})
+server.use(jwtCheck)
+server.use('/api', boardRouter)
+server.use('/api', columnRouter)
+server.use('/api', taskRouter)
+server.use('/api', subtaskRouter)
 
-// server.use(jwtCheck);
-server.post("/protected", (request, response) => {
-  console.log(request.body);
+server.post('/protected', (request, response) => {
+  console.log(request.body)
 
-  response.status(200).json({ message: "success" });
-});
-export default server;
+  response.status(200).json({message: 'success'})
+})
+export default server
