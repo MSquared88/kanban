c695b901a8169c76237cb8be4ae34c0bb54c0970
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-12-08 19:14:03 -0600

    change mobile view setup

--- app/src/components/desktop/SideBar.tsx
+++ app/src/components/desktop/SideBar.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import {LogoutButton} from '../LogOutButton'
 import ThemePicker from '../ThemePicker'
 
 export const SideBar = () => {
   return (
     <aside>
       <ThemePicker />
+      <LogoutButton />
     </aside>
   )
 }
