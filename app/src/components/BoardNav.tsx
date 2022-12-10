b64a6336d3f616e2970bcc091db46d0d7c538466
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-12-10 14:39:47 -0600

    add hooks file to utils folder

---
+++ app/src/components/BoardNav.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+import type {Board} from '../types'
+
+export default function BoardNav() {
+  return (
+    <nav>
+      <ul>
+        {/* {boards.map(board => {
+          return <li key={board.id}>{board.name}</li>
+        })} */}
+      </ul>
+    </nav>
+  )
+}
