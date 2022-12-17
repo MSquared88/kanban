d8d98f9f4d63c6fa6f1d3ad2cee806c0dcaa9ded
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-12-16 22:03:59 -0600

    fix cross icon

--- app/src/assets/icon-cross.tsx
+++ app/src/assets/icon-cross.tsx
@@ -6,14 +6,11 @@ const IconCross: React.FunctionComponent<SvgProps> = ({
   fill,
   className,
 }) => (
-  <svg
-    width={width}
-    height={height}
-    fill={fill}
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path stroke="#635FC7" strokeWidth="2" fill="none" d="M9 6 5 2 1 6" />
+  <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg">
+    <g fill="#828FA3" fillRule="evenodd">
+      <path d="m12.728 0 2.122 2.122L2.122 14.85 0 12.728z" />
+      <path d="M0 2.122 2.122 0 14.85 12.728l-2.122 2.122z" />
+    </g>
   </svg>
 )
 
